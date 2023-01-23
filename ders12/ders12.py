2278f6465343834fba50fcb6ebd19f72c3b5b332
msansal <mehmet.sansal1299@gmail.com>
2023-01-23 03:37:33 +0300

    ders 12 eklendi, ders 9 klasor icine alindi,.me uzantilari txt'ye cevrildi

---
+++ ders12/ders12.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/drone/camera1/image_raw",Image,self.callback)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
+
+
+def main(args):
+  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
